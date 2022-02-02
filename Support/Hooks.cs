654980406d60dba737bc982e5bb95b88ba73392f
abonje2000 <46799533+abonje2000@users.noreply.github.com>
2022-02-02 18:09:19 +0000

    Add files via upload

---
+++ Support/Hooks.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnowledgePoolAutomation.Drivers
+{
+    public class Hooks
+    {
+        public static IWebDriver driver;
+
+        [BeforeScenario("@tag1")]
+        public void BeforeScenarioWithTag()
+        {
+            driver = new ChromeDriver();
+
+            driver.Manage().Window.Maximize();
+
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            //driver.Quit();
+            //TODO: implement logic that has to run after executing each scenario
+        }
+    }
+}
